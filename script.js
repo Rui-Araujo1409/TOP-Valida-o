6785f9f1006053071d93d41e6a85fd6ad48e8a1d
Rui-Araujo1409 <rui1409@gmail.com>
2026-02-11 16:22:30 +0000

    acrescentar span para os erros

---
+++ script.js
@@ -0,0 +1,25 @@
+const formulário = document.querySelector("form");
+const email = document.querySelector("#email");
+const país = document.querySelector("#país");
+const cp = document.querySelector("#cp");
+const pass = document.querySelector("#pass");
+const passconfirm = document.querySelector("#passconfirm");
+
+
+
+formulário.addEventListener("input", (e) => {
+
+});
+
+email.addEventListener("input", (e) => {
+
+});
+
+país.addEventListener("input", (e) => {
+
+});
+
+cp.addEventListener("input", (e) => {
+
+});
+
